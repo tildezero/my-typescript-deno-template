c44bea929576d7f58ff09dbedd0d1ebf4d518f4d
zeromomentum <zero@tilde.team>
2021-09-25 15:13:23 -0500

    hjisgofdahlajkgdsgshadjlk;'

--- commands/translate.ts
+++ commands/translate.ts
@@ -9,6 +9,7 @@ export default class TranslateCommand extends Command {
         if (ctx.rawArgs.length < 2) return await ctx.message.channel.send("`.tr language query` language codes: https://envs.sh/6x.txt")
         const language = ctx.rawArgs[0]
         const query = ctx.rawArgs.slice(0).join(" ")
+        console.log(query)
         const req = await fetch(`https://lingva.ml/api/v1/auto/${language}/${query}`)
         const res = await req.json()
         await ctx.message.channel.send(res.translation)
