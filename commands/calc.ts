5fb9941dc14e71601d635441481d819d0bfbca9f
zeromomentum <zero@tilde.team>
2021-09-27 22:56:36 -0500

    how do numbers work

--- commands/calc.ts
+++ commands/calc.ts
@@ -8,7 +8,7 @@ export default class CalcCommand extends Command {
         const expr = encodeURIComponent(ctx.argString)
         const req = await fetch(`http://api.mathjs.org/v4/?expr=${expr}`)
         const res = await req.text()
-        if (req.status === 200) {
+        if (req.status === 400) {
             await ctx.message.channel.send(`Error: ${res}`)
         } else {
             await ctx.message.channel.send(res)
