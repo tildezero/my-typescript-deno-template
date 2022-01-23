07f0620b91ae4aa40d11ac48ae82ccf8456ff0d6
me <zero@tilde.team>
2022-01-23 17:08:18 -0600

    sb

--- commands/restart.ts
+++ commands/restart.ts
@@ -2,11 +2,11 @@ import { Command, CommandContext } from "../deps.ts";
 
 export default class PingCommand extends Command {
     name = "start"
-    description = "trolling"
+    description = "unbelievable amt of trolling"
     ownerOnly = true
 
     async execute(ctx: CommandContext) {
-        const r =  Deno.run({cmd: ['pm2', 'start', '3'], stdout: "piped", stderr: "piped"})
+        const r =  Deno.run({cmd: ['pm2', 'restart', '3'], stdout: "piped", stderr: "piped"})
         const s = await r.status()
         if (s.success) {
             const o = await r.output()
--- commands/stop.ts
+++ commands/stop.ts
@@ -2,20 +2,19 @@ import { Command, CommandContext } from "../deps.ts";
 
 export default class PingCommand extends Command {
     name = "start"
-    description = "trolling"
+    description = "more trolling"
     ownerOnly = true
 
     async execute(ctx: CommandContext) {
-        const r =  Deno.run({cmd: ['pm2', 'start', '3'], stdout: "piped", stderr: "piped"})
+        const r =  Deno.run({cmd: ['pm2', 'stop', '3'], stdout: "piped", stderr: "piped"})
         const s = await r.status()
         if (s.success) {
             const o = await r.output()
             await ctx.message.channel.send("success")
             await ctx.message.channel.send(`\`\`\`${o ?? "no stdout"}\`\`\``)
         } else {
-            const e = r.stderrOutput()
             await ctx.message.channel.send("fail")
-            await ctx.message.channel.send(`\`\`\`${e ?? "no stdout"}\`\`\``)
+            await ctx.message.channel.send(`\`\`\`${r.stderrOutput ?? "no stdout"}\`\`\``)
         }
     }
 }
